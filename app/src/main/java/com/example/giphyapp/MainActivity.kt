6173a771dc8d17d4062c44bd3944ab9878cb9c3d
zaidk22 <zaidk22@gmail.com>
2022-04-09 12:38:04 +0530

    Add Search View

--- app/src/main/java/com/example/giphyapp/MainActivity.kt
+++ app/src/main/java/com/example/giphyapp/MainActivity.kt
@@ -12,18 +12,19 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.*
 
 const val BASE_URL = "http://api.giphy.com/v1/"
 class MainActivity : AppCompatActivity() {
-
+private lateinit var tempArrayList : MutableList<DataObject>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
         val gifs = mutableListOf<DataObject>()
-
-        val adapter =GifAdapter(this,gifs)
+        tempArrayList = mutableListOf<DataObject>()
+        val adapter =GifAdapter(this,tempArrayList)
         recycler.adapter = adapter
         recycler.setHasFixedSize(true)
         recycler.layoutManager = LinearLayoutManager(this)
@@ -41,12 +42,44 @@ class MainActivity : AppCompatActivity() {
                     Log.d("msg","Failed")
                 }
 
-               gifs.addAll(body!!.res)
-
+                gifs.addAll(body!!.res)
+                tempArrayList.addAll(gifs)
                 adapter.notifyDataSetChanged()
 
+                searchView.setOnQueryTextListener(object :SearchView.OnQueryTextListener{
+                    override fun onQueryTextSubmit(query: String?): Boolean {
 
+
+                    return false
+                    }
+
+
+                    override fun onQueryTextChange(newText: String?): Boolean {
+                        tempArrayList.clear()
+                        val searchText = newText!!.toLowerCase(Locale.getDefault())
+                        if (searchText.isNotEmpty()) {
+                            tempArrayList.forEach {
+                                if (it.title.toLowerCase(Locale.getDefault())
+                                        .contains(searchText)
+                                ) {
+                                    tempArrayList.add(it)
+                                }
+                            }
+                            recycler!!.adapter!!.notifyDataSetChanged()
+
+                        }
+                        else{
+                            tempArrayList.clear()
+                            tempArrayList.addAll(gifs)
+                            recycler!!.adapter!!.notifyDataSetChanged()
+                        }
+                        return false
+
+                    }
+
+                })
             }
+
 
 
 
